b13cbffd88d76e7fcbd7d8fc754c0845c0b42f3e
n3xtchen <echenwen@gmail.com>
2025-05-14 15:32:35 +0800

    分析代理1.0：支持pytest（允许异步）

---
+++ tests/test_mcp_client.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright © 2025 n3xtchen <echenwen@gmail.com>
+#
+# Distributed under terms of the GPL-2.0 license.
+
+"""
+
+"""
+
+import asyncio
+
+import pytest
+
+from report_agent.mcp.client import MCPClient
+
+from mcp import StdioServerParameters
+
+server_params = StdioServerParameters(
+        command="python",
+        args=['tests/mcp_server/mcp_test.py'],
+        env=None,
+                    )
+
+@pytest.mark.asyncio
+async def test_mcp_tools():
+    async with MCPClient(server_params) as client:
+        tools = await client.get_available_tools()
+        print("xx")
+        assert len(tools)>0
